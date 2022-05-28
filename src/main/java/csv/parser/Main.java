53813902e0cfff32f312fd06281a38b851c589e0
Aadesh wagh <waghaadesh300@gmail.com>
2022-05-28 13:13:30 +0530

    added csv reader

---
+++ src/main/java/csv/parser/Main.java
@@ -0,0 +1,20 @@
+package csv.parser;
+
+import csv.parser.load.CSVLoader;
+import csv.parser.load.CSVLoaderImp;
+import csv.parser.read.CSVReader;
+import csv.parser.read.CSVReaderImp;
+
+public class Main {
+    static CSVLoader csvLoader;
+    public static void main(String[] args) {
+//        CSVLoader loader = new CSVLoaderImp();
+//        CSVSchema csvSchema = loader.LoadCSV("google_review_ratings.csv");
+//        CSVReader csv = new CSVReaderImp(csvSchema);
+//        System.out.println(csv.getColumn("Category 1").size());
+
+    }
+
+
+
+}
